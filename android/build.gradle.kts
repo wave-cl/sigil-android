7e3a8c535cd583675702a2b745a87c65dfe991af
c <wave-cl@pm.me>
2026-09-17 11:12:46 +0100

    Kotlin 2.3: what the UnifiedPush libraries were compiled with

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,5 +4,5 @@
 // Everything that decides anything is in Rust; see ../README.md.
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
